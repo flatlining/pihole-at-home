dc9c2cd45f985bf2e58c2844a8a75da3402f3d02
Matias Schertel <mschertel@gmail.com>
2016-12-04 14:20:26 -0200

    improve file handling

--- docker-run.sh
+++ docker-run.sh
@@ -4,6 +4,23 @@ IMAGE=${1:-'diginc/pi-hole:arm'}
 NIC=${2:-'eth0'}
 IP=$(ip addr show "$NIC" | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
 
+dir=$(pwd)/data
+if [[ ! -e $dir ]]; then
+    mkdir $dir
+fi
+dir=$(pwd)/data/var
+if [[ ! -e $dir ]]; then
+    mkdir $dir
+fi
+dir=$(pwd)/data/var/log
+if [[ ! -e $dir ]]; then
+    mkdir $dir
+fi
+if [ ! -f $dir/pihole.log ]
+then
+	touch $dir/pihole.log
+fi
+
 # Default ports + daemonized docker container
 docker run -p 53:53/tcp -p 53:53/udp -p 80:80 \
   --cap-add=NET_ADMIN \
